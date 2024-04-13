fd0cc9fbb8e8d5a46b72f1f4ccb81277faab1d67
Tran Minh Thang | thangtran.se <thangtran.se@gmail.com>
2024-04-13 22:59:16 +0700

    Update UI and fake api tags

--- web/app/components/TagAutocomplete.tsx
+++ web/app/components/TagAutocomplete.tsx
@@ -8,16 +8,26 @@ import Chip from "@mui/material/Chip";
 interface Props {
   tags: { id: string; tag: string }[];
   renderInputCustom: React.ReactNode | ((params: any) => React.ReactNode);
+  defaultValue: string[];
+  handleTagSelection?: (tags: string[]) => void;
 }
 
-const TagAutocomplete: React.FC<Props> = ({ tags, renderInputCustom }) => {
+const TagAutocomplete: React.FC<Props> = ({
+  tags,
+  renderInputCustom,
+  defaultValue,
+  handleTagSelection
+}) => {
   return (
     <Autocomplete
       multiple
       id="tags-filled"
       options={tags.map((_tag) => _tag.tag)}
-      defaultValue={[tags[1].tag]}
+      defaultValue={defaultValue}
       freeSolo
+      onChange={(event, value) => {
+        handleTagSelection?.(value)
+      }}
       renderTags={(value: readonly string[], getTagProps) =>
         value.map((label: string, index: number) => (
           <div key={label}>
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,102 @@
 "use client";
 
-import { Box, Container, TextField } from "@mui/material";
+import { Container, Skeleton, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import TagAutocomplete from "./components/TagAutocomplete";
 
+const API_ENDPOINT = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+const MOCK_USER_ID = 1;
+const MOCK_TAGS_ID = "default";
+const API_PATH_TAGS_RECORD_USER = `tags_record_user/${MOCK_USER_ID}`;
+const API_PATH_TAGS = `tags/${MOCK_TAGS_ID}`;
+
+async function fetchTags(
+  path: string = "",
+  method: string = "GET",
+  body: any = null
+) {
+  const options: any = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  const response = await fetch(`${API_ENDPOINT}/${path}`, options);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const tags = await response.json();
+  return tags;
+}
+
 export default function Home() {
+  const [tags, setTags] = useState<{ id: string; tag: string }[]>([]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    Promise.all([
+      fetchTags(API_PATH_TAGS_RECORD_USER),
+      fetchTags(API_PATH_TAGS),
+    ])
+      .then(([userTags, tags]) => {
+        setLoading(false);
+        setSelectedTags(userTags?.tags || []);
+        setTags(tags?.tags || []);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+      });
+  }, []);
+
+  const handleSubmitTagChangedSelection = (_tags: string[]) => {
+    // tìm tags mới thêm vào danh sách tags state
+    const newTags = _tags
+      .map((tag) => {
+        const found = tags.find((_tag) => _tag.tag === tag);
+        return !found && { id: Math.floor(Math.random() * 1000), tag };
+      })
+      .filter(Boolean);
+
+    Promise.all([
+      fetchTags(API_PATH_TAGS_RECORD_USER, "PUT", {
+        id: MOCK_USER_ID,
+        tags: _tags,
+      }),
+      fetchTags(API_PATH_TAGS, "PUT", {
+        id: MOCK_TAGS_ID,
+        tags: [...tags, ...newTags],
+      }),
+    ])
+      .then(() => {
+        console.info("Done!");
+      })
+      .catch((error) => console.error(error));
+  };
+
+  if (loading)
+    return (
+      <Container
+        sx={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Skeleton variant="rectangular" width={320} height={40} />
+      </Container>
+    );
+
   return (
     <Container
       sx={{
@@ -14,19 +107,19 @@ export default function Home() {
         justifyContent: "center",
       }}
     >
-      <Box>
-        <TagAutocomplete
-          tags={_mockData}
-          renderInputCustom={(params) => <TextField {...params} label="Tags" />}
-        />
-      </Box>
+      <TagAutocomplete
+        tags={tags}
+        defaultValue={selectedTags}
+        handleTagSelection={handleSubmitTagChangedSelection}
+        renderInputCustom={(params) => (
+          <TextField
+            fullWidth
+            {...params}
+            label="Tags"
+            style={{ minWidth: 120 }}
+          />
+        )}
+      />
     </Container>
   );
 }
-
-const _mockData: { id: string; tag: string }[] = [
-  { id: "1", tag: "test01" },
-  { id: "2", tag: "test02" },
-  { id: "3", tag: "tag01" },
-  { id: "4", tag: "tag02" },
-];
